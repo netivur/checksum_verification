df07701e724881b447d6141cc91baa8d4a0c25cd
IUNeterebskii <coolman26262@gmail.com>
2025-01-21 14:41:34 +0300

    Change include files and namespaces

--- sourcescsvp/maincsvp.cpp
+++ sourcescsvp/maincsvp.cpp
@@ -1,27 +1,28 @@
+#include <iostream>
 #include <vector>
 #include <memory>
-#include "Algo_handler.h"
+#include <fstream>
+#include "json_structure_handler.h"
 
-int main(int argc, char* argv[]){
-    if (sver::correct_input(argc, argv)) {
+int main(int argc, char* argv[]) {   
+    if (jsh::correct_input(argc, argv)) {
         try {
-            sver::json jsonarr;
-            std::ifstream inputstream;
+            jsh::json jsonarr;
+            std::ifstream inputstream;            
             inputstream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
             inputstream.open(argv[1]);
             inputstream >> jsonarr;
-	    inputstream.close();
+            inputstream.close();
 
-            std::unique_ptr< std::vector<sver::Sum_n_path>> uptr_to_vp =
-                std::make_unique<std::vector<sver::Sum_n_path>>();
+            std::unique_ptr< std::vector<jsh::Sum_n_path>> uptr_to_vp = std::make_unique<std::vector<jsh::Sum_n_path>>();
             *uptr_to_vp = jsonarr;
 
-            Algo_handler ah("crc32");
-            sver::set_algorithm(ah, argc, argv);
-
+            jsh::Json_struct_handler ah("crc32");
+            jsh::set_algorithm(ah, argc, argv);
+            
             for (auto it = uptr_to_vp->begin(); it != uptr_to_vp->end(); ++it) {
                 try {
-                    if (sver::the_file_exists((it->path_).c_str())) {
+                    if (jsh::the_file_exists((it->path_).c_str())) {
                         bool result = ah.use_method(it->path_, it->sum_);
                         std::cout << "File: " << it->path_ << "\n";
                         if (result) {
@@ -36,12 +37,12 @@ int main(int argc, char* argv[]){
                 catch (std::ifstream::failure& e) {
                     std::cerr<<e.what()<< '\n';
                 }
-            }
+            }            
         }
         catch (std::length_error& le) {
             std::cerr << le.what() << '\n';
             return EXIT_FAILURE;
-        }
+        }   
         catch (std::bad_alloc& bae) {
             std::cerr << bae.what() << '\n';
             return EXIT_FAILURE;
