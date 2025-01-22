0cf608f914de53c98f54e228b55d88a466be9cd8
IUNeterebskii <coolman26262@gmail.com>
2025-01-22 11:02:55 +0300

    Add new feature declaration

--- sourcescsvp/include/crc_functions.h
+++ sourcescsvp/include/crc_functions.h
@@ -2,9 +2,10 @@
 #include <unordered_map>
 #include <string>
 namespace crc_f {
-	unsigned long CRC32_function(unsigned char* buf, unsigned long len);
-	unsigned long CRC32_count(const std::string& filename);
+    std::shared_ptr<unsigned long[]> create_crc32_table();
+	unsigned long crc32(unsigned long crc, unsigned char* buffer, unsigned long long size, std::shared_ptr<unsigned long[]> crc_table);
+	std::ifstream::pos_type determine_file_size(const std::string& filename);
+	unsigned long calculate_checksum_crc32(const std::string& filename);	
 	bool check_sum_crc32(const std::string& address, const std::string& expect_res);
-	
 	extern std::unordered_map<std::string, bool(*)(const std::string&, const std::string&)> algorithms_map;
 }
